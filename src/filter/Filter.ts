447d09172a5dfd010f1a77aa7d8c55167d39c0f2
Fogrexon <fogrexon@gmail.com>
2021-08-21 20:53:28 +0900

    wip

--- src/filter/Filter.ts
+++ src/filter/Filter.ts
@@ -1,8 +1,74 @@
-import quadVertex from './glsl/quad.vert';
+import quadVertex from './glsl/quad.vs';
+
+export interface FilterRenderingInfo {
+  targetTexture: WebGLTexture;
+}
+
+const compileShader = (
+  gl: WebGLRenderingContext,
+  shader: WebGLShader,
+  source: string,
+) => {
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.warn(source);
+    throw new Error(<string>gl.getShaderInfoLog(shader));
+  }
+};
 
 class Filter {
-  constructor(fragmentShader: string) {
-    
+  private fragmentSource: string;
+
+  private gl: WebGLRenderingContext;
+
+  private width: number;
+
+  private height: number;
+
+  private framebuffer: WebGLFramebuffer;
+
+  private targetTexture: WebGLTexture;
+
+  private vertexShader: WebGLShader;
+
+  private fragmentShader: WebGLShader;
+
+  constructor(fragmentSource: string) {
+    this.fragmentSource = fragmentSource;
+  }
+
+  public init(gl: WebGLRenderingContext, width: number, height: number) {
+    this.gl = gl;
+
+    this.framebuffer = <WebGLFramebuffer>gl.createFramebuffer();
+    this.targetTexture = <WebGLTexture>gl.createTexture();
+    this.width = width;
+    this.height = height;
+  }
+
+  public render({targetTexture}: FilterRenderingInfo) {
+    const { gl } = this;
+
+    // frame buffer rendering
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, this.targetTexture);
+    gl.texImage2D(
+      gl.TEXTURE_2D, 0, gl.RGBA, this.width, this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null,
+    );
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.framebufferTexture2D(
+      gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.targetTexture, 0,
+    );
+
+    // set render texture
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, targetTexture);
   }
 }
 
--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -17,32 +17,29 @@ class Renderer {
     image.parentElement?.appendChild(this.canvas);
     image.parentElement?.removeChild(image);
 
-    this.gl = <WebGLRenderingContext>this.canvas.getContext('webgl');
+    this.gl = <WebGLRenderingContext> this.canvas.getContext('webgl');
   }
-  
+
   // copy image attrib to canvas
   private copyElementAttributes() {
     this.canvas.width = this.image.width;
     this.canvas.height = this.image.height;
-    this.image.classList.forEach(className => {
+    this.image.classList.forEach((className) => {
       this.canvas.classList.add(className);
     });
     this.canvas.id = this.image.id;
-    for (let key in this.image.style) {
-      this.canvas.style[key] = this.image.style[key];
-    }
-    for (let key in this.image.dataset) {
-      this.canvas.dataset[key] = this.image.dataset[key];
-    }
-
+    Object.entries(this.image.style).forEach(([key, value]) => {
+      this.canvas.style[key] = value;
+    });
+    Object.entries(this.image.dataset).forEach(([key, value]) => {
+      this.canvas.dataset[key] = value;
+    });
   }
 
   public render() {
     this.gl.clearColor(0, 0, 0, 0);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT || this.gl.DEPTH_BUFFER_BIT);
-
-    
-  };
+  }
 }
 
 export { Renderer };
