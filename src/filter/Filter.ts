423facd7bfe8fd9f759fe87b8ada41438b8c5b26
Fogrex <fogrexon@gmail.com>
2021-08-21 19:10:35 +0900

    wip

---
+++ src/filter/Filter.ts
@@ -0,0 +1,9 @@
+import quadVertex from './glsl/quad.vert';
+
+class Filter {
+  constructor(fragmentShader: string) {
+    
+  }
+}
+
+export { Filter };
--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -11,11 +11,38 @@ class Renderer {
 
   constructor({ image }: RendererParameter) {
     this.image = image;
+
     this.canvas = document.createElement('canvas');
+    this.copyElementAttributes();
     image.parentElement?.appendChild(this.canvas);
+    image.parentElement?.removeChild(image);
+
+    this.gl = <WebGLRenderingContext>this.canvas.getContext('webgl');
+  }
+  
+  // copy image attrib to canvas
+  private copyElementAttributes() {
+    this.canvas.width = this.image.width;
+    this.canvas.height = this.image.height;
+    this.image.classList.forEach(className => {
+      this.canvas.classList.add(className);
+    });
+    this.canvas.id = this.image.id;
+    for (let key in this.image.style) {
+      this.canvas.style[key] = this.image.style[key];
+    }
+    for (let key in this.image.dataset) {
+      this.canvas.dataset[key] = this.image.dataset[key];
+    }
+
   }
 
-  public render();
+  public render() {
+    this.gl.clearColor(0, 0, 0, 0);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT || this.gl.DEPTH_BUFFER_BIT);
+
+    
+  };
 }
 
 export { Renderer };
