b61b5e11879631b7d18994d60d601d3417a3d169
Fogrex <fogrexon@gmail.com>
2021-08-21 19:10:28 +0900

    setting

---
+++ rollup.config.js
@@ -0,0 +1,60 @@
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import typescript from '@rollup/plugin-typescript';
+
+const extensions = ['.ts', '.js'];
+
+export default [
+
+  {
+    input: 'src/index.ts',
+
+    preserveModules: true,
+
+    output: {
+      dir: 'build/commonjs',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+
+    plugins: [
+      postcss({
+        extract: true,
+      }),
+      babel({
+        extensions,
+      }),
+      typescript({
+        declaration: true,
+        rootDir: 'src',
+        declarationDir: 'build/commonjs',
+      }),
+    ],
+  },
+
+  {
+    input: 'src/index.ts',
+    preserveModules: true,
+    output: {
+      dir: 'build/es6',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    },
+
+    plugins: [
+      postcss({
+        extract: true,
+      }),
+      babel({
+        extensions,
+      }),
+      typescript({
+        declaration: true,
+        rootDir: 'src',
+        declarationDir: 'build/es6',
+      }),
+    ],
+  },
+];
