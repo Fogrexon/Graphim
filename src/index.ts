c66d1266625dc6c560e521281a9383c665f720e9
Fogrex <fogrexon@gmail.com>
2021-11-03 21:17:52 +0900

    index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+// filter nodes
+export { GraphimNode, RenderID, CanvasID, ForwardingData, RenderSetting } from './filter/GraphimNode';
+export { DefaultInput } from './filter/DefaultInput'
+export { MiddleNode } from './filter/MiddleNode';
 export { Filter } from './filter/Filter';
+export { BlendNode } from './filter/BlendNode';
+export { BlendFilter } from './filter/BlendFilter';
+export { DelayNode } from './filter/DelayNode';
+
 export * as Primitives from './filter/primitives/Primitives';
 export { UniformSetter } from './filter/UniformSetter';
 export { Renderer } from './renderer/Renderer';
