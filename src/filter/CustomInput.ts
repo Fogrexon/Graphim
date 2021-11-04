8d659117b55b0045544e8591849178d2e480bff9
Fogrex <fogrexon@gmail.com>
2021-11-04 22:41:57 +0900

    custominput initialized

--- src/filter/CustomInput.ts
+++ src/filter/CustomInput.ts
@@ -14,10 +14,10 @@ export abstract class CustomInput extends GraphimNode {
 
   // eslint-disable-next-line no-unused-vars
   public init(gl: WebGLRenderingContext, canvasID: CanvasID) {
+    
     this.gl = gl;
     this.renderResult.targetTexture = <WebGLTexture>gl.createTexture();
     bindTexture(gl, this.renderResult.targetTexture, this.image);
-    this.initialized = canvasID;
   }
 
   public setImage(image: HTMLImageElement) {
@@ -27,13 +27,10 @@ export abstract class CustomInput extends GraphimNode {
 
   public release() {
     this.gl?.deleteTexture(this.renderResult.targetTexture);
+    this.initialized = '';
   }
 
   public render(setting: RenderSetting): void {
-    if (!this.gl || !this.initialized || setting.canvasID !== this.initialized) {
-      this.init(setting.gl, setting.canvasID);
-      if(!this.gl) throw new Error('gl is not initialized');
-    }
     // pass the input texture
     this.renderResult.renderID = setting.renderID;
     if(setting.renderToCanvas) console.warn('DefaultInput cannot be destination node.');
